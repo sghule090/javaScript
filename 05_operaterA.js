0d7ff860b2aa7766d640c4eabc77656b9d72abfa
sghule090 <sghule090@gmail.com>
2023-05-15 21:30:08 +0530

    Create 05_operaterA.js

---
+++ 05_operaterA.js
@@ -0,0 +1,10 @@
+
+function squareOfWordLength(argu1) {
+var splitString=argu1.split(" ");
+var totalString=splitString.length();
+console.log(`the total no of length is:->${totalString} `);
+    
+}
+squareOfWordLength("javaSrcipt");
+squareOfWordLength("developer smart");
+squareOfWordLength("google chrome");
